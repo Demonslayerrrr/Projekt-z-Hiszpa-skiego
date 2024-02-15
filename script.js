2e0fddf00bae733378a4251a961a7b658afe7a11
gofrywplynie29 <144332042+gofrywplynie29@users.noreply.github.com>
2024-02-15 16:21:32 +0100

    ilia dodaj tekst

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-document.addEventListener("DOMContentLoaded", function () {
-  alert("Bienvenidos a la página sobre Varsovia!");
-});
+// document.addEventListener("DOMContentLoaded", function () {
+//   alert("Bienvenidos a la página sobre Varsovia!");
+// });
